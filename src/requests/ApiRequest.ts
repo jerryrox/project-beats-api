8ec11a95dfd568169cd87ab9fc5b140b21d30a51
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 10:41:32 +1200

    Parse accessToken from ApiRequest if exists.

--- src/requests/ApiRequest.ts
+++ src/requests/ApiRequest.ts
@@ -3,9 +3,12 @@ import express from 'express';
 export default class ApiRequest {
 
     req: express.Request;
+    accessToken: string | undefined;
 
 
     constructor(req: express.Request) {
         this.req = req;
+
+        this.accessToken = req.body.accessToken;
     }
 }
