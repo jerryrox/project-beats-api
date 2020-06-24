6b594ad15e6538dd6ff505d3768134154f83b9df
Jerry Kim <jerryrox@live.co.kr>
2020-06-24 21:22:02 +1200

    Added DateUtils

---
+++ src/__test__/DateUtils.test.ts
@@ -0,0 +1,40 @@
+import DateUtils from '../utils/DateUtils';
+
+describe("DateUtils", () => {
+    test("getTimeAfter", () => {
+        const curDate = new Date();
+        expect(DateUtils.getTimeAfter(1)).toBe(curDate.setSeconds(curDate.getSeconds() + 1));
+
+        const customDate = new Date("1997-03-27");
+        expect(DateUtils.getTimeAfter(2, customDate)).toBe(customDate.setSeconds(customDate.getSeconds() + 2));
+    });
+
+    test("isExpired", () => {
+        const curDate = new Date();
+        const pastDate = new Date();
+        pastDate.setDate(curDate.getDate() - 1);
+        const futureDate = new Date();
+        futureDate.setDate(curDate.getDate() + 1);
+
+        expect(DateUtils.isExpired(pastDate)).toBeTruthy();
+        expect(DateUtils.isExpired(curDate)).toBeTruthy();
+        expect(DateUtils.isExpired(futureDate)).toBeFalsy();
+
+        expect(DateUtils.isExpired(curDate, pastDate)).toBeFalsy();
+        expect(DateUtils.isExpired(curDate, futureDate)).toBeTruthy();
+    });
+    test("isExpired with time and date", () => {
+        const curDate = new Date().getTime();
+        let pastDate = new Date().getTime();
+        pastDate -= 1000;
+        const futureDate = new Date();
+        futureDate.setTime(curDate + 86500);
+
+        expect(DateUtils.isExpired(pastDate)).toBeTruthy();
+        expect(DateUtils.isExpired(curDate)).toBeTruthy();
+        expect(DateUtils.isExpired(futureDate)).toBeFalsy();
+
+        expect(DateUtils.isExpired(curDate, pastDate)).toBeFalsy();
+        expect(DateUtils.isExpired(curDate, futureDate)).toBeTruthy();
+    });
+});
