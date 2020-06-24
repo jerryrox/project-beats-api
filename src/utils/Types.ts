9d2d8ca08272bb5d607bbb5981c4e83358d66caf
Jerry Kim <jerryrox@live.co.kr>
2020-06-24 18:41:29 +1200

    Create Types.ts

---
+++ src/utils/Types.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+/**
+ * Types of supported Node runtime mode.
+ */
+export enum NodeEnv {
+    Development = "development",
+    Production = "production",
+    Test= "test"
+}
+
+/**
+ * Types of API providers currently supported by Project: Beats.
+ */
+export enum ApiProvider {
+    Osu = "osu",
+    Bloodcat = "bloodcat"
+}
+
+/**
+ * Types of response which the client application should check and react accordingly.
+ */
+export enum ResponseType {
+    Success = "Success",
+    Unsupported = "Unsupported",
+    Error = "Error",
+
+    RequireLogin = "RequireLogin",
+    RequireOAuth = "RequireOAuth",
+}
+
+/**
+ * Type of the function for Express route handler.
+ */
+export type ExpressRoute = (req: express.Request, res: express.Response) => void;
