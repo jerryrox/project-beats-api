d4b5e84286723cb0aae5b4ed3b3a9afc4e1143c2
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 16:44:20 +1200

    Update MapsetsFormatter.ts

--- src/utils/formats/MapsetsFormatter.ts
+++ src/utils/formats/MapsetsFormatter.ts
@@ -1,8 +1,36 @@
 import { IMapset, IMap } from '../Types';
+import Table from '../Table';
+
 /**
- * Formatter which standardizes maps and mapsets' format.
+ * Formatter which standardizes maps/mapsets' format and converts query parameters to be compatible between API providers and pb-api.
  */
 export default abstract class MapsetsFormatter {
+
+    /**
+     * Game mode converter between pb-api and osu.
+     */
+    abstract readonly modeConverter: Table;
+
+    /**
+     * Mapset category converter between pb-api and osu.
+     */
+    abstract readonly categoryConverter: Table;
+
+    /**
+     * Mapset genre converter between pb-api and osu.
+     */
+    abstract readonly genreConverter: Table;
+
+    /**
+     * Mapset sort converter between pb-api and osu.
+     */
+    abstract readonly sortConverter: Table;
+
+    /**
+     * Mapset language converter between pb-api and osu.
+     */
+    abstract readonly languageConverter: Table;
+    
 
     /**
      * Formats the specified data into a mapset.
