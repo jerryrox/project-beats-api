befb8353d29a1f2097afbc02b5a96a7d0bd83f80
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 16:46:16 +1200

    Fixed test due to potential error caused by slow system.

--- src/__test__/DateUtils.test.ts
+++ src/__test__/DateUtils.test.ts
@@ -3,7 +3,9 @@ import DateUtils from '../utils/DateUtils';
 describe("DateUtils", () => {
     test("getTimeAfter", () => {
         const curDate = new Date();
-        expect(DateUtils.getTimeAfter(1)).toBe(curDate.setSeconds(curDate.getSeconds() + 1));
+        const timeAfterSecond = DateUtils.getTimeAfter(1);
+        // Be generous to give at most 1 second off.
+        expect(timeAfterSecond).toBeLessThanOrEqual(curDate.setSeconds(curDate.getSeconds() + 2));
 
         const customDate = new Date("1997-03-27");
         expect(DateUtils.getTimeAfter(2, customDate)).toBe(customDate.setSeconds(customDate.getSeconds() + 2));
@@ -40,7 +42,7 @@ describe("DateUtils", () => {
 
     test("getUnixTime", () => {
         const curDate = new Date();
-        expect(DateUtils.getUnixTime()).toBe(Math.floor(curDate.getTime() / 1000));
+        expect(DateUtils.getUnixTime()).toBeLessThanOrEqual(Math.floor(curDate.getTime() / 1000) + 1);
         expect(DateUtils.getUnixTime(new Date(Date.UTC(1970, 0)))).toBe(0);
     });
 });
