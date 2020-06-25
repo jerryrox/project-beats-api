b856dd2ec33fb96338624a7aff232474271c4796
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 17:16:39 +1200

    Implemented mapset search query for bloodcat

--- src/api/bloodcat/BloodcatMapsets.ts
+++ src/api/bloodcat/BloodcatMapsets.ts
@@ -5,18 +5,32 @@ import ErrorResponse from '../../responses/ErrorResponse';
 import BloodcatApi from "./BloodcatApi";
 import MapsetsResponse from '../../responses/MapsetsResponse';
 import BloodcatMapsetsFormatter from './formats/BloodcatMapsetsFormatter';
+import MapsetsRequest from '../../requests/MapsetsRequest';
+import StringUtils from '../../utils/StringUtils';
 
 export async function mapsets(req: express.Request, res: express.Response) {
+    const request = new MapsetsRequest(res);
 
     try {
+        const formatter = new BloodcatMapsetsFormatter();
+
+        const status = formatter.categoryConverter.getValue(request.category);
+        const mode = formatter.modeConverter.getValue(request.mode);
+        const genre = formatter.genreConverter.getValue(request.genre);
+        const language = formatter.languageConverter.getValue(request.language);
+        const page = StringUtils.tryParseNumber(request.cursorValue, 1);
         const response = await axios.get(
-            `${BloodcatApi.baseUrl}?mod=json`
+            `${BloodcatApi.baseUrl}?mod=json&c=b&s=${status}&m=${mode}&g=${genre}&l=${language}&p=${page}`
         );
 
-        const formatter = new BloodcatMapsetsFormatter();
+        const parsedMapsets: any[] = response.data.map((m: any) => formatter.formatMapset(m));
+        const cursor = parsedMapsets.length === 0 ? null : {
+            "cursor[page]": page + 1
+        };
+
         res.json(new MapsetsResponse({
-            mapsets: response.data.map((m: any) => formatter.formatMapset(m)),
-            cursor: null // TODO: Null if no mapsets. Otherwise, next page number.
+            mapsets: parsedMapsets,
+            cursor
         }));
     }
     catch (e) {
