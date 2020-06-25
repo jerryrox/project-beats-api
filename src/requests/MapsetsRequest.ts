ff7216597dfe24b912046c2362451cee1ecaf94f
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 17:09:13 +1200

    Explicitly specify default value when certain mapset search filter is missing

--- src/requests/MapsetsRequest.ts
+++ src/requests/MapsetsRequest.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import ApiRequest from './ApiRequest';
 import StringUtils from '../utils/StringUtils';
+import { GameModeType, MapsetCategoryType, MapsetGenreType, MapsetLanguageType, MapsetSortType } from '../utils/Types';
 
 export default class MapsetsRequest extends ApiRequest {
 
@@ -23,11 +24,11 @@ export default class MapsetsRequest extends ApiRequest {
 
         this.cursorId = req.query?.cursorId;
         this.cursorValue = this.findCursorValue(req.query);
-        this.mode = StringUtils.tryParseNumber(req.query?.mode);
-        this.category = StringUtils.tryParseNumber(req.query?.category);
-        this.genre = StringUtils.tryParseNumber(req.query?.genre);
-        this.language = StringUtils.tryParseNumber(req.query?.language);
-        this.sort = StringUtils.tryParseNumber(req.query?.sort);
+        this.mode = StringUtils.tryParseNumber(req.query?.mode, GameModeType.OsuStandard);
+        this.category = StringUtils.tryParseNumber(req.query?.category, MapsetCategoryType.Any);
+        this.genre = StringUtils.tryParseNumber(req.query?.genre, MapsetGenreType.Any);
+        this.language = StringUtils.tryParseNumber(req.query?.language, MapsetLanguageType.Any);
+        this.sort = StringUtils.tryParseNumber(req.query?.sort, MapsetSortType.Ranked);
         this.query = req.query?.query;
         this.hasVideo = req.query?.hasVideo === "true" || req.query?.hasVideo === true;
         this.hasStoryboard = req.query?.hasStoryboard === "true";
