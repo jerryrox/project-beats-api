ec3299e13c75519b6b5361b2e15365fb42683d5b
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 17:13:14 +1200

    Allow undefined parameter

--- src/utils/Table.ts
+++ src/utils/Table.ts
@@ -13,28 +13,34 @@ export default class Table {
     /**
      * Returns whether the specified key exists in the table.
      */
-    containsKey(key: TableRecordType) {
+    containsKey(key: TableRecordType | undefined) {
+        if (key === undefined) {
+            return false;
+        }
         return Object.keys(this.obj).indexOf(String(key)) >= 0;
     }
 
     /**
      * Returns whether the specified value exists in the table.
      */
-    containsValue(value: any) {
+    containsValue(value: any | undefined) {
         return Object.values(this.obj).indexOf(value) >= 0;
     }
 
     /**
      * Returns the value for specified key.
      */
-    getValue(key: TableRecordType): any | undefined {
+    getValue(key: TableRecordType | undefined): any | undefined {
+        if (key === undefined) {
+            return undefined;
+        }
         return this.obj[key];
     }
 
     /**
      * Returns the first encountered key for specified value.
      */
-    getKey(value: any) {
+    getKey(value: any | undefined) {
         const keys = Object.keys(this.obj);
         for (let i = 0; i < keys.length; i++) {
             if (this.obj[keys[i]] === value) {
