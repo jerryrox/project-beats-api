7a84b193e10dce5640875e143140ecfe61f916a2
Jerry Kim <jerryrox@live.co.kr>
2020-06-25 15:35:45 +1200

    Update OsuApi.test.ts

--- src/__test__/osu/OsuApi.test.ts
+++ src/__test__/osu/OsuApi.test.ts
@@ -1,12 +1,11 @@
 import OsuApi from '../../api/osu/OsuApi';
 import ApiRequest from '../../requests/ApiRequest';
-import express from 'express';
 
 describe("OsuApi", () => {
     test("getHeaderWithAuth", () => {
         const request = new ApiRequest({
-            body: {
-                accessToken: "heyeyeyeyey"
+            headers: {
+                authorization: "Bearer heyeyeyeyey"
             }
         });
         let header = OsuApi.getHeaderWithAuth(request, {
