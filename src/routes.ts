46f0ac58e5e98f10b50ac9822473b0bcee9b00e3
Jerry Kim <jerryrox@live.co.kr>
2020-06-28 22:53:48 +1200

    Added test routes for the client side.

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import ApiGateway from './api/ApiGateway';
+import SuccessResponse from './responses/SuccessResponse';
+import ErrorResponse from './responses/failures/ErrorResponse';
 
 const routes = express.Router();
+
+routes.get("/test/success", (req, res) => {
+    res.json(new SuccessResponse({
+        data: req.query,
+        message: "Test success"
+    }));
+});
+routes.get("/test/fail", (req, res) => {
+    res.json(new ErrorResponse(new Error("Test fail")));
+});
 
 routes.post("/:provider/auth", (req, res) => {
     ApiGateway.getApi(req.params.provider).auth(req, res);
