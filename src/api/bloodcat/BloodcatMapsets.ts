0082d51ecf555367e8b8639ccfc7f084c8f1995b
Jerry Kim <jerryrox@live.co.kr>
2020-06-29 12:26:08 +1200

    Debugging

--- src/api/bloodcat/BloodcatMapsets.ts
+++ src/api/bloodcat/BloodcatMapsets.ts
@@ -14,6 +14,8 @@ export async function mapsets(req: express.Request, res: express.Response) {
         const request = new MapsetsRequest(req);
         const formatter = new BloodcatMapsetsFormatter();
 
+        console.log(`Bloodcat request url: ${formatter.getMapsetSearchUrl(request)}`);
+
         const response = await axios.get(
             formatter.getMapsetSearchUrl(request)
         );
